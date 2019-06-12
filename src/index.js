6a09563f8701f5c1ae8c9fe44bf75bff0ccffc1b
李广川 <testman@hotmail.com>
2019-06-12 14:39:45 +0800

    new

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
+import printMe from './project.js'
 
-fuction component(){
-	let element = document.cretaeElement("div")
-	element.innerHTML = _.join(['Hello','webpack'], ' ')
+function component(){
+	let element = document.createElement("div")
+	element.innerHTML = _.join(['Hello','webpack','爱心小兔字体'], ' ')
 	
-	return element
+	element.classList.add('box')
+
+	// 新增点击按钮
+	let btn = document.createElement('button');
+	btn.innerHTML = '点击我会打印信息';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
+
+	return element 
 }
 
 document.body.appendChild(component())
